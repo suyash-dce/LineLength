6f0b29213e3ef3dee1768fd9a929cc35ef9c795a
Suyash Jain <jain.suyash28@gmail.com>
2020-09-26 11:56:30 +0530

    Added: Line Generator and Length Calculator is Added

--- LineLength.java
+++ LineLength.java
@@ -2,5 +2,28 @@ public class LineLength {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line Comparison Program !!");
+		int l1x1 = RandomPoint();
+		int l1x2 = RandomPoint();
+		int l1y1 = RandomPoint();
+		int l1y2 = RandomPoint();
+		int l2x1 = RandomPoint();
+		int l2x2 = RandomPoint();
+		int l2y1 = RandomPoint();
+		int l2y2 = RandomPoint();
+		
+		double line1 = CalcLen(l1x1,l1x2,l1y1,l1y2);
+		String Length1 = Double.toString(line1);
+		double line2 = CalcLen(l2x1,l2x2,l2y1,l2y2);
+		String Length2 = Double.toString(line2);
+		System.out.println("Length of Line 1 is: "+Length1);
+		System.out.println("Length of Line 2 is: "+Length2);
+		}
+		public static int RandomPoint() {
+		return (int)Math.floor(Math.random()*100);
+		}
+		public static double CalcLen(int x1,int x2,int y1,int y2) {
+		return Math.sqrt(((int)Math.pow((x2-x1), 2)+(int)Math.pow((x2-x1), 2)));
+		
 		}
 	}
+		
